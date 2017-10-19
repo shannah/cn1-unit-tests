39615d01829946f2ff2a2c5f73663bac70f875f3
Steve Hannah <steve@weblite.ca>
2017-10-19 13:46:19 -0700

    Updated gitignore.  Added some tests for Container.getComponentAt(int,int)

--- tests/core-com-codename1-ui-Component/test/com/codename1/ui/TestComponent.java
+++ tests/core-com-codename1-ui-Component/test/com/codename1/ui/TestComponent.java
@@ -5,7 +5,13 @@
  */
 package com.codename1.ui;
 
+import com.codename1.components.ToastBar;
+import com.codename1.maps.Coord;
 import com.codename1.testing.AbstractTest;
+import com.codename1.testing.TestUtils;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.LayeredLayout;
 
 /**
  *
@@ -15,8 +21,105 @@ public class TestComponent extends AbstractTest {
 
     @Override
     public boolean runTest() throws Exception {
-        this.assertBool(true);
+        
+        
+        getComponentAt_int_int();
         return true;
+    }
+
+    @Override
+    public boolean shouldExecuteOnEDT() {
+        return true;
+    }
+    
+    
+    private void getComponentAt_int_int() {
+        getComponentAt_int_int_button();
+        getComponentAt_int_int_label();
+        getComponentAt_int_int_container();
+        getComponentAt_int_int_browsercomponent();
+    }
+    
+    
+    
+    
+    private void getComponentAt_int_int_button() {
+        log("Testing getComponentAt(x, y) with button");
+        int w = Display.getInstance().getDisplayWidth();
+        int h = Display.getInstance().getDisplayHeight();
+        
+        Form f = new Form("My Form", new BorderLayout());
+        Label l = new Button("Hello");
+        f.add(BorderLayout.CENTER, l);
+        
+        f.show();
+        TestUtils.waitForFormTitle("My Form");
+        Component middleComponent = f.getComponentAt(w/2, h/2);
+        assertEqual(l, middleComponent, "Found wrong component");
+    }
+    
+    private void getComponentAt_int_int_label() {
+        log("Testing getComponentAt(x, y) with label");
+        int w = Display.getInstance().getDisplayWidth();
+        int h = Display.getInstance().getDisplayHeight();
+        
+        Form f = new Form("My Form", new BorderLayout());
+        Label l = new Label("Hello");
+        f.add(BorderLayout.CENTER, l);
+        
+        f.show();
+        TestUtils.waitForFormTitle("My Form");
+        Component middleComponent = f.getComponentAt(w/2, h/2);
+        assertEqual(l, middleComponent, "Found wrong component");
+        
+        
+    }
+    
+    
+    
+    
+    private void getComponentAt_int_int_container() {
+        int w = Display.getInstance().getDisplayWidth();
+        int h = Display.getInstance().getDisplayHeight();
+        
+        Form f = new Form("My Form", new BorderLayout());
+        Container l = new Container();
+        f.add(BorderLayout.CENTER, l);
+        
+        f.show();
+        TestUtils.waitForFormTitle("My Form");
+        Component middleComponent = f.getComponentAt(w/2, h/2);
+        assertEqual(l, middleComponent, "Found wrong component");
+    }
+    
+    private void getComponentAt_int_int_browsercomponent() {
+        int w = Display.getInstance().getDisplayWidth();
+        int h = Display.getInstance().getDisplayHeight();
+        Form mapDemo = new Form("Maps", new LayeredLayout());
+        Toolbar.setOnTopSideMenu(true);
+        Toolbar tb = new Toolbar();
+        mapDemo.setToolbar(tb);
+        mapDemo.setTitle("Maps");
+        tb.addCommandToSideMenu(new Command("Test") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //testNestedScrollingLabels();
+            }
+        });
+        BrowserComponent mc = new BrowserComponent();
+        mapDemo.add(mc);
+        
+        mapDemo.show();
+        
+        TestUtils.waitForFormTitle("Maps");
+        Component middleComponent = mapDemo.getComponentAt(w/2, h/2);
+        assertTrue(mc == middleComponent || mc.contains(middleComponent),  "Wrong component found in middle. Expected "+mc+" but found "+middleComponent);
+    
+        tb.showToolbar();
+        TestUtils.waitFor(500);
+        Component res = mapDemo.getComponentAt(10, h/2);
+        assertTrue(tb.contains(res), "Toolbar is open so getComponentAt() should return something on the toolbar.  But received "+res+".  Toolbar is "+tb);
+    
     }
     
 }
