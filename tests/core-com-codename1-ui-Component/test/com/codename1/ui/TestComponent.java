a4159f54fab21f00ec2b6004f245d74dd168e926
Steve Hannah <steve@weblite.ca>
2017-10-19 10:51:45 -0700

    Initial commit

---
+++ tests/core-com-codename1-ui-Component/test/com/codename1/ui/TestComponent.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codename1.ui;
+
+import com.codename1.testing.AbstractTest;
+
+/**
+ *
+ * @author shannah
+ */
+public class TestComponent extends AbstractTest {
+
+    @Override
+    public boolean runTest() throws Exception {
+        this.assertBool(true);
+        return true;
+    }
+    
+}
